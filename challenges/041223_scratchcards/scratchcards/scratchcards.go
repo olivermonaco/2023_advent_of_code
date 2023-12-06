dee428b2ba04039c36594937d0283407c9802ff4
Oliver Monaco <oliverjmonaco@gmail.com>
2023-12-05 20:07:34 -0500

    scratch version part two

--- challenges/041223_scratchcards/scratchcards/scratchcards.go
+++ challenges/041223_scratchcards/scratchcards/scratchcards.go
@@ -33,6 +33,14 @@ func CalculatePartOne(ctx context.Context, input []string) int {
 	return result
 }
 
+func CalculatePartTwo(ctx context.Context, input []string) int {
+	// ctx = log.Logger.WithContext(ctx)
+	// l := log.Ctx(ctx).With().Logger()
+
+	_, _, result := winnersNum(ctx, 0, input)
+	return result
+}
+
 func toIntMap(strs []string) map[int]struct{} {
 	ret := make(map[int]struct{}, len(strs))
 	for _, s := range strs {
@@ -53,4 +61,31 @@ func separateNums(s string) (map[int]struct{}, map[int]struct{}) {
 
 	return toIntMap(haveNumsSl), toIntMap(winningNumsSl)
 
+}
+
+func winnersNum(ctx context.Context, curIdx int, lines []string) (int, int, int) {
+	if curIdx == len(lines) {
+		return 0, 0, 0
+	}
+
+	haveNums, winningNums := separateNums(lines[curIdx])
+	var numWinners *int
+	for k := range haveNums {
+		if _, ok := winningNums[k]; ok {
+			numWinners = kit.Ptr(kit.Deref(numWinners) + 1)
+			// l.Info().Int("winning_num", k).Msg("")
+		}
+	}
+	var lineResult int
+	if numWinners != nil {
+		lineResult = max(int(math.Pow(float64(2), float64(*numWinners-1))), 1)
+	}
+	numCopies := kit.Deref(numWinners)
+	resultWithCopies := lineResult
+	for i := 0; i <= numCopies; i += 1 {
+		nthLineResult, _, _ := winnersNum(ctx, curIdx+i, lines)
+		resultWithCopies += nthLineResult
+	}
+	_, _, nextTotal := winnersNum(ctx, curIdx+1, lines)
+	return lineResult, resultWithCopies, resultWithCopies + nextTotal
 }
