818edae885209261acf432fdd340d9b05e90848d
Oliver Monaco <oliverjmonaco@gmail.com>
2023-12-19 11:57:46 -0500

    adds bones for day 10

---
+++ challenges/101223_pipe_maze/pipe_maze/pipe_maze_test.go
@@ -0,0 +1,49 @@
+package pipe_maze
+
+import (
+	"context"
+	"testing"
+
+	"github.com/olivermonaco/2023_advent_of_code/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPipeMaze_PartOne(t *testing.T) {
+	ctx := context.Background()
+
+	tests := []struct {
+		inputFilename string
+		expOutput     int
+	}{
+		{
+			inputFilename: "test_files/example_input.txt",
+			expOutput:     0,
+		},
+	}
+
+	for _, tt := range tests {
+		data := kit.ReadFileConstructLines(ctx, tt.inputFilename)
+		actual := CalculatePartOne(ctx, data)
+		assert.Equalf(t, tt.expOutput, actual, "inequal expected:%d\nand actual:\n%d")
+	}
+}
+
+func TestPipeMaze_PartTwo(t *testing.T) {
+	ctx := context.Background()
+
+	tests := []struct {
+		inputFilename string
+		expOutput     int
+	}{
+		{
+			inputFilename: "test_files/example_input.txt",
+			expOutput:     0,
+		},
+	}
+
+	for _, tt := range tests {
+		data := kit.ReadFileConstructLines(ctx, tt.inputFilename)
+		actual := CalculatePartTwo(ctx, data)
+		assert.Equalf(t, tt.expOutput, actual, "inequal expected:%d\nand actual:\n%d")
+	}
+}
